ceca24b0e4361fa3ab699ca849fbb419050e22a2
FloydWatson <floydbcseit@gmail.com>
2020-04-17 16:36:07 +1200

    config changes

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,17 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', '_YOUR_USER_');
-define('DB_PASS', '_YOUR_PASS_');
-define('DB_NAME', '_YOUR_DBNAME_');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'phpwebapp');
 
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL root
-define('URLROOT', 'http://localhost/watsonmvc');
+define('URLROOT', 'http://localhost/phpwebapp');
 
 // Site Name
 // Change this to your new name
-define('SITENAME', 'WatsonMVC');
+define('SITENAME', 'Ecommerce Store');
