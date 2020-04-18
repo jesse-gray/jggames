cd2ec3676f23e3de356470489cf81c6356c9b779
FloydWatson <floydbcseit@gmail.com>
2020-04-18 23:33:30 +1200

    added welcome

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -22,7 +22,7 @@
         <!-- Show account and logout if logged in -->
         <?php if (isset($_SESSION['user_id'])) : ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>/pages/Index">Account</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/pages/Index">Welcome <?php echo $_SESSION['user_name']; ?></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
